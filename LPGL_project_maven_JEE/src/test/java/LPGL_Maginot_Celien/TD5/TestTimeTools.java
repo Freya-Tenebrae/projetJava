da0ce60705cf028ccd72ecb527c7805c39ba150b
Célia <celien.maginot@orange.fr>
2020-05-06 22:55:21 +0200

    2nd commit

---
+++ LPGL_project_maven_JEE/src/test/java/LPGL_Maginot_Celien/TD5/TestTimeTools.java
@@ -0,0 +1,42 @@
+package LPGL_Maginot_Celien.TD5;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+import Tools.TimeTools;
+
+public class TestTimeTools {
+	
+	@Test
+	public void testValidSecondOrMinuteNumberOver59() {
+		assertFalse(TimeTools.validSecondOrMinute(66));
+	}
+	
+	@Test
+	public void testValidSecondOrMinuteNumberUnder0() {
+		assertFalse(TimeTools.validSecondOrMinute(-42));
+	}
+	
+	@Test
+	public void testValidSecondOrMinuteValidNumber() {
+		assertTrue(TimeTools.validSecondOrMinute(42));
+	}
+	
+	@Test
+	public void testValidHeureNumberOver23() {
+		assertFalse(TimeTools.validHour(42));
+	}
+	
+	@Test
+	public void testValidHeureNumberUnder0() {
+		assertFalse(TimeTools.validHour(-07));
+	}
+	
+	@Test
+	public void testValidHeureValidNumber() {
+		assertTrue(TimeTools.validHour(07));
+	}
+
+}
