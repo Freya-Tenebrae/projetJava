2e8f020d42df2e1db1e8431110a1084df397afdb
Célia <celien.maginot@orange.fr>
2020-05-07 13:15:04 +0200

    Commit 15 - app non fonctionelle

--- LPGL_project_maven_JEE/src/main/java/LPGL_Maginot_Celien/Project_06_05_2020/App.java
+++ LPGL_project_maven_JEE/src/main/java/LPGL_Maginot_Celien/Project_06_05_2020/App.java
@@ -3,6 +3,7 @@ package LPGL_Maginot_Celien.Project_06_05_2020;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 import java.util.Scanner;
+import java.util.regex.Pattern;
 
 import Metier.Banque;
 
@@ -19,12 +20,14 @@ public class App  implements KeyListener
     	System.out.println("Ceci est un petit programme qui va permettre de consulter les fonctionnalités d'une banque");	
     	System.out.println("L'identifiant s'effectuer par le nom du client : veuillez rentrer le nom d'un client pour continuer (Célia/Freya/Mael)");
     	System.out.println("Puis sélectionner le compte à consulter en tapant son identifiant (une liste sera afficher)");
-    	System.out.println("Pour effectuer un dépot rentrer le symbole + suivi de l'identifant de compte, et enfin du montan de la transaction");
-    	System.out.println("Pour effectuer un retrait rentrer le symbole + suivi de l'identifant de compte, et enfin du montan de la transaction");
+    	System.out.println("Pour effectuer un dépot rentrer le symbole +");
+    	System.out.println("Pour effectuer un retrait rentrer le symbole -");
+    	System.out.println("après cela il sera nécessaire de taper l'identifiant du compte sur lequel on souhaite faire l'opération");
+    	System.out.println("suivi d'un point '.' et de la la somme souhaités");
     	System.out.println("Pour changer d'utilisateur il est nécessaire de redémarer l'application en pressant echap");
     	do {
     		value = sc.next();
-    	} while (value != "Célia" || value != "Freya" || value != "Mael")
+    	} while (value != "Célia" || value != "Freya" || value != "Mael");
     	b.ouverture_compte(value);
     	
     	while (infLoop == true); {
@@ -33,9 +36,17 @@ public class App  implements KeyListener
     		{
     			b.consultation(value);
     		}
-    		else if (value == -* || value == +* )   	
+    		else if (value == "-")   	
     		{
-    			
+    			value = sc.next();
+    			String[]v = value.split(Pattern.quote("."));
+    			b.retrait(v[0], Double.parseDouble(v[1]));
+    		}
+    		else if (value == "+")   	
+    		{
+    			value = sc.next();
+    			String[]v = value.split(Pattern.quote("."));
+    			b.depot(v[0], Double.parseDouble(v[1]));
     		}
     	}
     	
